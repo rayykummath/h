21bdeb268297aa578f3f2f7247a5dc4d6abeb426
rayykummath <73564492+rayykummath@users.noreply.github.com>
2021-10-11 12:30:55 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -22,3 +22,20 @@ cla=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/2ZaHAF8U
 function Modal_loded(){
     console.log("MOOOOOOOOOOODDDDDDDDAAAAAAAAAAALLLLLLLLL LLLLLLLLOOOOOOOODDDDDDDDDDDEEEEEEDDDDDDDDDDDDDD");
 }
+
+function Idnt_object(){
+    Pic=document.getElementById("result");
+    cla.classify(Pic, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        prediction=results[0].label;
+        accuracy=results[0].confidence;
+        document.getElementById("object_name").innerHTML=prediction;
+        document.getElementById("accuracy_num").innerHTML=confidence.ToFixed(2)*100+"%";
+    }
+    }
